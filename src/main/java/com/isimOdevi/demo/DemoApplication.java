6610ebb729e967758736f96e0913650a713d6229
KarakusAlican <tr.alikarakus2008@gmail.com>
2023-08-18 15:42:54 +0300

    bu dosya tam olarak bitmedi

---
+++ src/main/java/com/isimOdevi/demo/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.isimOdevi.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DemoApplication {
+	public static void toplama(int a,int b,int c){
+		System.out.println("toplmları = " +  (a+b+c));
+	}
+	public static void selamlama(String isim) {
+		System.out.println("selam " + isim);
+	}
+	public static void main(String[] args) {
+		/*("murat");
+		//SpringApplicatin.run(DemoApplication.class, args);*/
+		toplama(3,2,5);
+	}
+
+}
